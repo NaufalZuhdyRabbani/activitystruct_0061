25e6f25c416405f1269fde06bb0eb21a72ea5fa7
NaufalZuhdyRabbani <naufalkzrz@gmail.com>
2024-04-01 15:28:58 +0700

    display nama dan alamat mahasiswa

--- PemrogramanDasarStrukturB/PemrogramanDasarStrukturB.cpp
+++ PemrogramanDasarStrukturB/PemrogramanDasarStrukturB.cpp
@@ -14,5 +14,9 @@ int main()
 	Mahasiswa mhs;
 	cout << "Nomor Mahasiswa : ";
 	cin >> mhs.nim;
+	cout << "Nama Mahasiswa : ";
+	cin >> mhs.nama;
+	cout << "Alamat Mahasiswa : ";
+	cin >> mhs.alamat;
 	
 }
